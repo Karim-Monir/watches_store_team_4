dbc091396dd352861c127fa58df102a68e0fccbc
Karim Monir <karim.monir1992@gmail.com>
2023-08-28 23:15:33 +0300

    cart added

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CartPage extends StatefulWidget {
@@ -11,365 +12,143 @@ class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: 390,
-          height: 844,
-          clipBehavior: Clip.antiAlias,
-          decoration: ShapeDecoration(
-            color: const Color(0xFFF8F8F8),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 24, right: 25),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 52,
             ),
-          ),
-          child: Stack(
-            children: [
-              Positioned(
-                left: 92,
-                top: 89,
-                child: Container(
-                  width: 206,
-                  height: 315,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage("https://via.placeholder.com/206x315"),
-                      fit: BoxFit.cover,
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color(0x3F000000),
-                        blurRadius: 4,
-                        offset: Offset(0, 9),
-                        spreadRadius: 0,
-                      )
-                    ],
+            Row(
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    CupertinoIcons.back,
+                    size: 20,
                   ),
                 ),
-              ),
-              Positioned(
-                left: 0,
-                top: 473,
-                child: Container(
-                  width: 390,
-                  height: 371,
-                  decoration: const ShapeDecoration(
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40),
-                        topRight: Radius.circular(40),
-                        bottomLeft: Radius.circular(19),
-                        bottomRight: Radius.circular(19),
-                      ),
-                    ),
-                  ),
+                const SizedBox(
+                  width: 84,
                 ),
-              ),
-              Positioned(
-                left: 80,
-                top: 444,
-                child: Container(
-                  width: 62,
-                  height: 52,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: 0,
-                        top: 0,
-                        child: Container(
-                          width: 62,
-                          height: 52,
-                          decoration: ShapeDecoration(
-                            color: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            shadows: [
-                              const BoxShadow(
-                                color: Color(0x3F000000),
-                                blurRadius: 4,
-                                offset: Offset(0, 4),
-                                spreadRadius: 0,
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 8,
-                        top: 3,
-                        child: Container(
-                          width: 47,
-                          height: 46,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image: NetworkImage("https://via.placeholder.com/47x46"),
-                              fit: BoxFit.fill,
-                            ),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Color(0x3F000000),
-                                blurRadius: 4,
-                                offset: Offset(0, 4),
-                                spreadRadius: 0,
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 249,
-                top: 441,
-                child: Container(
-                  width: 62,
-                  height: 57,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: 0,
-                        top: 3,
-                        child: Container(
-                          width: 62,
-                          height: 52,
-                          decoration: ShapeDecoration(
-                            color: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            shadows: [
-                              const BoxShadow(
-                                color: Color(0x3F000000),
-                                blurRadius: 4,
-                                offset: Offset(0, 4),
-                                spreadRadius: 0,
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 12,
-                        top: 0,
-                        child: Container(
-                          width: 39,
-                          height: 57,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image: NetworkImage("https://via.placeholder.com/39x57"),
-                              fit: BoxFit.fill,
-                            ),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Color(0x3F000000),
-                                blurRadius: 4,
-                                offset: Offset(0, 4),
-                                spreadRadius: 0,
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 157,
-                top: 436,
-                child: Container(
-                  width: 77,
-                  height: 68,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: 0,
-                        top: 0,
-                        child: Container(
-                          width: 77,
-                          height: 68,
-                          decoration: ShapeDecoration(
-                            color: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            shadows: [
-                              const BoxShadow(
-                                color: Color(0x3F000000),
-                                blurRadius: 4,
-                                offset: Offset(0, 4),
-                                spreadRadius: 0,
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 17,
-                        top: 0,
-                        child: Container(
-                          width: 43,
-                          height: 68,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image: NetworkImage("https://via.placeholder.com/43x68"),
-                              fit: BoxFit.cover,
-                            ),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Color(0x3F000000),
-                                blurRadius: 4,
-                                offset: Offset(0, 4),
-                                spreadRadius: 0,
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const Positioned(
-                left: 23,
-                top: 614,
-                child: SizedBox(
-                  width: 343,
-                  height: 84,
+                const Center(
                   child: Text(
-                    'The Rolex collection offers a wide range of prestigious, high-precision timepieces, from Professional to Classic models to suit any wrist',
+                    'My order',
                     style: TextStyle(
-                      color: Color(0xB21F1F1F),
-                      fontSize: 16,
+                      color: Color(0xFF1F1F1F),
+                      fontSize: 28,
                       fontFamily: 'Inter',
-                      fontWeight: FontWeight.w400,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            SizedBox(
+              height: 417,
+              child: ListView.separated(
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 341,
+                    height: 122,
+                    decoration: ShapeDecoration(
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      shadows: const [
+                        BoxShadow(
+                          color: Color(0x3F000000),
+                          blurRadius: 4,
+                          offset: Offset(0, 4),
+                          spreadRadius: 0,
+                        )
+                      ],
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) => const SizedBox(
+                  height: 20,
                 ),
+                itemCount: 9,
               ),
-              const Positioned(
-                left: 23,
-                top: 539,
-                child: Text(
-                  'Rolex Black watch',
+            ),
+            const SizedBox(
+              height: 47,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Order Summary',
                   style: TextStyle(
                     color: Color(0xFF1F1F1F),
                     fontSize: 24,
                     fontFamily: 'Inter',
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 24,
-                top: 579,
-                child: Container(
-                  height: 20,
-                  child: Stack(
-                    children: [
-                      const Positioned(
-                        left: 20,
-                        top: 1,
-                        child: Text(
-                          '4.9',
-                          style: TextStyle(
-                            color: Color(0xE51F1F1F),
-                            fontSize: 16,
-                            fontFamily: 'Inter',
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 0,
-                        top: 0,
-                        child: Container(
-                          width: 20,
-                          height: 20,
-                          clipBehavior: Clip.antiAlias,
-                          decoration: const BoxDecoration(),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 23,
-                top: 742,
-                child: Container(
-                  width: 158,
-                  height: 54,
-                  decoration: ShapeDecoration(
-                    shape: RoundedRectangleBorder(
-                      side: const BorderSide(width: 0.50, color: Color(0xFF025464)),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 203,
-                top: 742,
-                child: Container(
-                  width: 163,
-                  height: 54,
-                  decoration: ShapeDecoration(
-                    color: const Color(0xFF025464),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ),
-              ),
-              const Positioned(
-                left: 233,
-                top: 755,
-                child: Text(
-                  'Buy Now',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontFamily: 'Inter',
                     fontWeight: FontWeight.w600,
                   ),
                 ),
-              ),
-              const Positioned(
-                left: 79,
-                top: 755,
-                child: Text(
-                  '\$99',
+              ],
+            ),
+            const SizedBox(
+              height: 19,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Subtotal',
+                  style: TextStyle(
+                    color: Color(0xE51F1F1F),
+                    fontSize: 20,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  '\$248',
                   style: TextStyle(
                     color: Color(0xFF025464),
-                    fontSize: 24,
+                    fontSize: 20,
                     fontFamily: 'Inter',
-                    fontWeight: FontWeight.w600,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 48,
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 39,
+                padding: const EdgeInsets.all(10),
+                decoration: ShapeDecoration(
+                  color: const Color(0xFF025464),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Checkout',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
                 ),
               ),
-              Positioned(
-                left: 22,
-                top: 52,
-                child: Container(
-                  width: 20,
-                  height: 20,
-                  clipBehavior: Clip.antiAlias,
-                  decoration: const BoxDecoration(),
-                  child: Stack(children: []),
-                ),
-              ),
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'product_details.dart';
+import 'cart_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ProductDetails(),
+      home: CartPage(),
     );
   }
 }
